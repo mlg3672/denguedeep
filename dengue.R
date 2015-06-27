1693b40bd5c37ddb480c16d2a5fc73e2f446d6fb
mlg3672 <michele.goe@gmail.com>
2015-06-27 20:30:33 +0800

    expanded dataset

--- dengue.R
+++ dengue.R
@@ -4,19 +4,28 @@ setwd("~/Documents/Python-Projects/dengue")
 ## read dengue csv into dataframe
 cases<-read.csv("dengue.csv", colClasses="character")
 
+# clean data ----------------
 ## set column names
 colnames(cases)<- c("year","week","state","city","address","cases","duration")
 cases<-cases[,1:7]
 dim(cases)
+
+
 ##subset cases
 subcases<-cases[sample(nrow(cases), 300), ]
 
+# data transformations -----
 ## make a new data columns with start and stop dates
 library(lubridate)
 subcases$start<-as.Date(paste(subcases$year,"01","01",sep="-")) + as.numeric(subcases$week)*7
 subcases$end<- subcases$start + as.numeric(subcases$duration)
 subcases$month<-month(subcases$start)
-
+#logcases
+#new entry for each case
+df<-data.frame()
+#colnames(df)<-c("lon", "lat","year","week","state","city","address")
+df<- subcases[rep(row.names(subcases), subcases$cases), 1:7]
+hist(as.numeric(df$week),breaks=52)
 ## use location columns to find lat long location
 library(ggmap)
 library(plyr)
@@ -25,6 +34,8 @@ subcases<-cbind(gisdf,subcases)
 subcases<-subcases[order(subcases$cases),]
 means<-tapply(as.numeric(subcases$cases),subcases$state,mean)
 counts<-tapply(as.numeric(subcases$cases),subcases$state,count)
+
+# explore data ---------------------
 ## plot cases using lat long, weight with number of cases
 qplot(rownames(means),means)
 plot(subcases$week,subcases$cases)
@@ -32,6 +43,7 @@ plot(subcases$year,subcases$cases)
 plot(subcases$duration,subcases$cases)
 plot(subcases$lat,subcases$cases)
 plot(subcases$lon,subcases$lat)
+
 ## explore relationship between number of cases, duration, location
 
 ## overlap with external data - weather, precipatition, population, age ranges, density
